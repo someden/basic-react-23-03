10fe214a135dc38e1cef56aca7cd28b03d3cc236
Denis <kisonic@mail.ru>
2018-04-11 21:28:30 +0300

    rewrite articles to key -> value

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,12 +1,18 @@
-import { normalizedArticles as defaultArticles } from '../fixtures'
+import { normalizedArticles } from '../fixtures'
 import { DELETE_ARTICLE } from '../constants'
+
+const defaultArticles = normalizedArticles.reduce((acc, article) => ({
+    ...acc,
+    [article.id]: article
+}), {})
 
 export default (articlesState = defaultArticles, action) => {
     const { type, payload } = action
 
     switch (type) {
         case DELETE_ARTICLE:
-            return articlesState.filter(article => article.id !== payload.id)
+            const { [payload.id]: deletedArticle, ...restArticles } = articlesState;
+            return restArticles
 
         default:
             return articlesState
